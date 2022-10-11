52c44f3e9742b65574f8790959a04046491dc35c
osobytes <oscar@osobytes.com>
2022-10-11 13:29:19 -0700

    remove auth guard from routing

--- Client/src/api/Functions/BaseTask.cs
+++ Client/src/api/Functions/BaseTask.cs
@@ -11,7 +11,7 @@ namespace Taskify.Api.Functions
   {
     protected static async Task<T> ParseBodyAsync<T>(HttpRequest req)
     {
-      string? jsonBody = null;
+      string jsonBody = null;
       using (var sr = new StreamReader(req.Body, Encoding.UTF8))
       {
         jsonBody = await sr.ReadToEndAsync();
--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './shared/interceptors/auth.guard';
 import { TasksComponent } from './tasks/tasks.component';
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
-  {path:'', component: TasksComponent, canActivate: [AuthGuard]},
-  {path:'tasks', component: TasksComponent, canActivate: [AuthGuard]}
+  {path:'', component: TasksComponent},
+  {path:'tasks', component: TasksComponent}
 ];
 
 @NgModule({
